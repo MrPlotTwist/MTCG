5d5f78a588aca3f91478c16e79822bb6b2882242
tomi <tomi2002@gmx.at>
2024-10-15 23:28:48 +0200

    Neue Klassen geaddet, sowie Tests hinzugefügt

---
+++ MTCG/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int port = 10001;
+        Server server = new Server(port);
+        server.start();
+    }
+}
